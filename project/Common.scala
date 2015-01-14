3a05b7a9c6c287cbace57f2b5924066c2e95f927
Mike Limansky <mike.limansky@gmail.com>
2015-01-15 00:27:17 +0300

    Upgrade to Scala 2.11.5.

--- project/Common.scala
+++ project/Common.scala
@@ -4,8 +4,8 @@ import sbt.Keys._
 object Common {
   val settings = Seq(
     version := "0.4-SNAPSHOT",
-    scalaVersion := "2.11.4",
-    crossScalaVersions := Seq("2.11.4", "2.10.4"),
+    scalaVersion := "2.11.5",
+    crossScalaVersions := Seq("2.11.5", "2.10.4"),
     scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature"),
     libraryDependencies <++= scalaVersion { sv =>
       CrossVersion.partialVersion(sv) match {
